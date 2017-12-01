a1538e748a5fb402684f5c112036de8bc6d13de3
yogeshasok <yogeshasok@gmail.com>
2017-12-01 10:51:11 +0530

    TO test without DB

--- Server.js
+++ Server.js
@@ -1,51 +1,51 @@
 var express= require("express");
 var app=express();
-var MongoClient = require("mongodb").MongoClient;
-var ObjectId = require('mongodb').ObjectID
-var db=null;
+// var MongoClient = require("mongodb").MongoClient;
+// var ObjectId = require('mongodb').ObjectID
+// var db=null;
 var bodyparser=require('body-parser');
 
 app.use(bodyparser.json());
 app.use(express.static(__dirname+"/public"));
 
-MongoClient.connect("mongodb://10.201.47.68:27017/meeting",function(err,database){
-   // console.log(err);
-    db = database;
-   // console.log(db);
-});
+// MongoClient.connect("mongodb://10.201.47.68:27017/meeting",function(err,database){
+//    // console.log(err);
+//     db = database;
+//    // console.log(db);
+// });
 
 app.get("/list",function (req,res) {
     console.log(db);
-    db.collection("meet").find({}).toArray(function(err,doc){
-        console.log(err);
-        res.json(doc);
-    })
+    // db.collection("meet").find({}).toArray(function(err,doc){
+    //     console.log(err);
+    //     res.json(doc);
+    // })
 })
 
 app.post("/list",function(req,res){
     console.log(req.body);
-    db.collection("meet").insertOne(req.body,function(err,doc){
-        res.json(doc);
-    })
+    // db.collection("meet").insertOne(req.body,function(err,doc){
+    //     res.json(doc);
+    // })
 })
 
 app.delete("/list/:id",function(req,res){
     console.log(req.params.id+"fdfsffsd");
     var id=req.params.id;
     var myquery={_id: ObjectId(id)};
-    db.collection("meet").deleteOne(myquery, function(err, doc) {
-        res.json(doc);
-    });
+    // db.collection("meet").deleteOne(myquery, function(err, doc) {
+    //     res.json(doc);
+    // });
 
 })
 
 app.get("/list/:id",function (req,res) {
     console.log(req.params.id);
     var id=req.params.id;
-    db.collection("meet").findOne({_id: ObjectId(id)},function(err, doc) {
-           console.log(err);
-           res.json(doc);
-    })
+    // db.collection("meet").findOne({_id: ObjectId(id)},function(err, doc) {
+    //        console.log(err);
+    //        res.json(doc);
+    // })
 
 });
 app.put("/list/:id",function (req,res) {
@@ -53,10 +53,10 @@ app.put("/list/:id",function (req,res) {
     var id=req.params.id;
     var myquery={_id:ObjectId(id)};
     var newvalues={name:req.body.name,date:req.body.date,place:req.body.place,des:req.body.des};
-    db.collection("meet").updateOne(myquery,newvalues,function (err,doc) {
-        console.log(err);
-        res.json(doc);
-    })
+    // db.collection("meet").updateOne(myquery,newvalues,function (err,doc) {
+    //     console.log(err);
+    //     res.json(doc);
+    // })
 })
 
 app.listen(3000);
